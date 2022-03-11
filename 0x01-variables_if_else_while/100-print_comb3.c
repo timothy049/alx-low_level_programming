7d2680709f493579ff85437f29ebcb5a504a02ad
timothy049 <commandsec1996@gmail.com>
2022-03-11 11:56:39 -0800

    variable 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,13 +17,13 @@ int main(void)
 
 	while (i < 58)
 	{
-		j = 48;
+		j = i + 1;
 		while (j < 58)
 		{
 			putchar(i);
 			putchar(j);
 
-			if (i < 57 || j < 57)
+			if (i < 56 || j < 57)
 			{
 				putchar(44);
 				putchar(32);
