23cbb4360bea6c75c27daa95026e488d243275b7
timothy049 <commandsec1996@gmail.com>
2022-03-15 10:20:40 -0700

    faith 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,18 @@
 #include "main.h"
 
 /**
- * main - prints _putchar
- *
- * Description: prints _putchar
- *
- * Return: 0 (Success)
+ * main -  _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char text[10] = "_putchar";
-	int i = 0;
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(text[i]);
-	}
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
