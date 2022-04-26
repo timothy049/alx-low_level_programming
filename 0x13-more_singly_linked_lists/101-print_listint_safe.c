327ed4c3d9f60fde08b363535311c9378152047c
timothy049 <commandsec1996@gmail.com>
2022-04-26 02:48:48 -0700

    more singly linked lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,56 +1,30 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
-/**
- *unique_node_count - counts no. of unique nodes
- *@head:pointer to a head node
- *Return:number of unique nodes,otherwise 0
- */
-size_t unique_node_count(const listint_t *head)
-{
-	listint_t *hare, *tortoise;
-	size_t count = 1;
-
-	if (head == NULL || head->next == NULL)
-	{
-		return (0);
-	}
-	hare = head->next->next;
-	tortoise = head->next;
-	while (hare)
-	{
-		if (tortoise == hare)
-		{
-			tortoise = head;
-			while (tortoise != hare)
-			{
-				count++;
-				tortoise = tortoise->next;
-				hare = hare->next;
-			}
-			tortoise = tortoise->next;
-			while (tortoise != hare)
-			{
-				count++;
-				tortoise = tortoise->next;
-			}
 
 /**
- *print_listint_safe - prints a listint list
- *@head:pointer to head
- *Return:number of nodes in list
+ * print_listint_safe - prints a linked list, safely
+ * @head: list of type listint_t to print
+ *
+ * Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	unsigned int i = 0;
+	size_t num = 0;
+	long int diff;
 
-	if (head == NULL)
-		exit(98);
 	while (head)
 	{
-		printf("[%p] %d", (void *)head, head->n);
-		i++;
-		head = head->next;
+		diff = head - head->next;
+		num++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (diff > 0)
+			head = head->next;
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
+		}
 	}
-	return (i);
+
+	return (num);
 }
+
