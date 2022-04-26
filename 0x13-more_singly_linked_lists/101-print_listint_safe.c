f0dc6b581cb7acf601a0e3be4b862bb86cbcd65d
timothy049 <commandsec1996@gmail.com>
2022-04-26 02:44:46 -0700

    more singly linked lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,6 +2,40 @@
 #include <stdio.h>
 #include "lists.h"
 /**
+ *unique_node_count - counts no. of unique nodes
+ *@head:pointer to a head node
+ *Return:number of unique nodes,otherwise 0
+ */
+size_t unique_node_count(const listint_t *head)
+{
+	listint_t *hare, *tortoise;
+	size_t count = 1;
+
+	if (head == NULL || head->next == NULL)
+	{
+		return (0);
+	}
+	hare = head->next->next;
+	tortoise = head->next;
+	while (hare)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				count++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				count++;
+				tortoise = tortoise->next;
+			}
+
+/**
  *print_listint_safe - prints a listint list
  *@head:pointer to head
  *Return:number of nodes in list
@@ -14,7 +48,7 @@ size_t print_listint_safe(const listint_t *head)
 		exit(98);
 	while (head)
 	{
-		printf("[%p] %d", head, head->n);
+		printf("[%p] %d", (void *)head, head->n);
 		i++;
 		head = head->next;
 	}
